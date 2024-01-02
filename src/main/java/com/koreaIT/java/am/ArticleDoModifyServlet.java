7d24903146d4ff711c215055b861abd80e240707
devCJH <http-@naver.com>
2024-01-02 21:36:24 +0900

    Config파일로 DB접속정보 분리

--- src/main/java/com/koreaIT/java/am/ArticleDoModifyServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleDoModifyServlet.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.koreaIT.java.am.config.Config;
 import com.koreaIT.java.am.util.DBUtil;
 import com.koreaIT.java.am.util.SecSql;
 
@@ -26,9 +27,8 @@ public class ArticleDoModifyServlet extends HttpServlet {
 		Connection conn = null;
 		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/JSPAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
-			conn = DriverManager.getConnection(url, "root", "");
+			Class.forName(Config.getDBDriverName());
+			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPassword());
 			
 			String title = request.getParameter("title");
 			String body = request.getParameter("body");
